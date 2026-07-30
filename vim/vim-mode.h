ad09b1a1967c68444ba7bb86ed895a6bc04f5be1
Yaroslav Kargapolov <bit69tream@tuta.io>
2026-07-30 03:34:22 +0300

    Ignore visual line hooks when not in visual line mode

--- vim/vim-mode.h
+++ vim/vim-mode.h
@@ -163,7 +163,12 @@ void vim_esc(EditorCtx* ctx, VimProcessorState* vim_state) {
 
 void vim_visual_line_nav_up_pre_hook(EditorCtx* ctx, VimProcessorState* vim_state) {
   EditorCmd cmd = {0};
-  
+
+  if (!vim_state->visual_line_mode)
+  {
+    return;
+  }
+
   if (vim_state->line_distance_to_selection_start == 0)
   {
     // we are on the same line where we started the selection
@@ -179,7 +184,12 @@ void vim_visual_line_nav_up_pre_hook(EditorCtx* ctx, VimProcessorState* vim_stat
 
 void vim_visual_line_nav_down_post_hook(EditorCtx* ctx, VimProcessorState* vim_state) {
   EditorCmd cmd = {0};
-  
+
+  if (!vim_state->visual_line_mode)
+  {
+    return;
+  }
+
   if (vim_state->line_distance_to_selection_start == -1)
   {
     // we just moved to the same line where we started the selection
